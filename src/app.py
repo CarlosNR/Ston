4ad382f23dddf364244c31e7b9cc4d8233d9a3c7
Carlos Nunes <carlostaekwon@gmail.com>
2023-02-25 20:06:02 -0300

    credita e atualiza preço com feedback

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, session
+from flask import Flask, request, render_template, session, redirect
 from flask_session import Session
 from src.db import *
 from datetime import date
@@ -18,12 +18,12 @@ def not_found(e):
 def index():
     return render_template('/public/index.html')
 
-# pré input
+# Clientes
 @app.route('/insert/cliente')
 def cliente():
     return render_template('/public/clientes/cadastraCliente.html')
 
-# pós inputs
+# 
 @app.route('/insert/cliente', methods=['POST', 'GET'])
 def cadastraClientes():
 
@@ -56,21 +56,32 @@ def readCliente2():
 
 @app.route('/read/clientes')
 def listaClientes():
+    if("mensagemCreditar" in session):
+      mensagem = session["mensagemCreditar"]  
+      del session["mensagemCreditar"]
+      listaClientes = achaClientes()
+      return render_template('/public/clientes/listaClientes.html', listaClientes=listaClientes, mensagem=mensagem)
+
     listaClientes = achaClientes()
     return render_template('/public/clientes/listaClientes.html', listaClientes=listaClientes)
 
 @app.route('/update/cliente/credito/<int:id>/<float:credito>', methods=['POST', 'GET'])
-def credita1(id, credito):
+def creditar1(id, credito):
     
     return render_template('/public/clientes/credita.html', id=id, credito=credito)
 
 @app.route('/update/cliente/credito/<int:id>', methods=['POST', 'GET'])
-def credita2(id):
+def creditar2(id):
 
   creditoAdicional = request.form['creditoAdicional']
-  adicionaCredito(id, creditoAdicional)  
-  print(creditoAdicional)
-  return "Creditado com sucesso."
+  #fetchone retorna vetor msm se tiver apenas 1 item
+  saldo = adicionaCredito(id, creditoAdicional) 
+  print()
+  print(saldo)
+  print()
+  session["mensagemCreditar"] = "O cliente de id %s teve seus creditos adicionados em %.2f e está com %.2f de saldo" % (id, float(creditoAdicional), float(saldo[0]))
+
+  return redirect('/read/clientes')
 
 @app.route('/delete/cliente/<idApagado>', methods=['POST', 'GET'])
 def excluiCliente(idApagado):
@@ -78,11 +89,11 @@ def excluiCliente(idApagado):
   listaClientes = achaClientes()
   return render_template('/public/clientes/listaClientes.html', idApagado=idApagado, listaClientes=listaClientes)
 
+#Jogos
 @app.route('/insert/jogo/pt1')
 def jogo1():
     return render_template('/public/jogos/cadastraJogo1.html')
 
-# pós inputs
 @app.route('/insert/jogo/pt1', methods=['POST', 'GET'])
 def insereJogo1():
  
@@ -106,15 +117,12 @@ def insereJogo1():
       mensagem = "Jogo cadastrado com sucesso."
       return render_template('/public/jogos/cadastrajogo1.html', mensagem=mensagem)
 
-
 @app.route('/insert/jogo/pt2')
 def jogo2():
     return render_template('/public/jogos/cadastrajogo2.html')
 
-# pós inputs
 @app.route('/insert/jogo/pt2', methods=['POST', 'GET'])
 def cadastraJogos():
-
 
   nome = session["nome"]
   publicadora = session["publicadora"]
@@ -125,15 +133,41 @@ def cadastraJogos():
   insereJogo(nome, publicadora, maior18, genero, preco)  
   mensagem = "Jogo cadastrado com sucesso."
 
-  #if: session só for usado em insere jogo, session clear sera usado, 
-  #else: deletar campo a campo
-
-  # session["nome"] = None
-  # session["publicadora"] = None
-  # session["maior18"] = None
-  # session["genero"] = None
-  # session["preco"] = None
-
-  session.clear()
+  del session["nome"]
+  del session["publicadora"]
+  del session["maior18"]
+  del session["genero"]
+  del session["preco"]
 
   return render_template('/public/jogos/cadastrajogo1.html', mensagem=mensagem)
+
+@app.route('/read/jogos')
+def listaJogos():
+    if("mensagemPrecificar" in session):
+      mensagem = session["mensagemPrecificar"]  
+      del session["mensagemPrecificar"]
+      listaJogos = achaJogos()
+      return render_template('/public/jogos/listaJogos.html', listaJogos=listaJogos, mensagem=mensagem)
+      
+    listaJogos = achaJogos()
+    return render_template('/public/jogos/listaJogos.html', listaJogos=listaJogos)
+
+@app.route('/update/jogo/preco/<int:id>', methods=['POST', 'GET'])
+def precificar1(id):
+    jogo = achaJogoId(id)
+    return render_template('/public/jogos/precifica.html', id=id, preco=jogo[5])
+
+@app.route('/update/jogo/preco/<int:id>/<float:preco>', methods=['POST', 'GET'])
+def precificar2(id,preco):
+
+  novoPreco = request.form['novoPreco']
+  atualizaPrecoJogo(id, novoPreco) 
+  jogo = achaJogoId(id)
+  session["mensagemPrecificar"] = "O jogo %s teve seu valor modificado de %.2f para %.2f" % (jogo[2], float(preco), float(novoPreco))
+  return redirect('/read/jogos')
+
+@app.route('/delete/jogo/<idApagado>', methods=['POST', 'GET'])
+def excluiJogo(idApagado):
+  deletaJogo(idApagado)
+  listaJogos = achaJogos()
+  return render_template('/public/jogos/listaJogos.html', idApagado=idApagado, listaJogos=listaJogos)
--- src/db.py
+++ src/db.py
@@ -24,8 +24,9 @@ def deletaCliente(id):
     cursor.execute("delete from clientes where id=%s;",(id, ))
     conn.commit()
 def adicionaCredito(id, credito):
-    cursor.execute("update clientes set credito = (credito + %s) where id = %s;",(credito, id))
+    cursor.execute("update clientes set credito = (credito + %s) where id = %s returning credito;",(credito, id))
     conn.commit()
+    return cursor.fetchone()
 def achaClientes():
     cursor.execute("select * from clientes;")
     return cursor.fetchall()
@@ -38,34 +39,31 @@ def insereJogo(nome, publicadora, maior18, genero, preco):
 
     cursor.execute("insert into jogos (nome, publicadora, maior18, genero, preco) values (%s, %s, %s, %s, %s);",(nome, publicadora, maior18, genero, preco))
     conn.commit()
-
-def atualizaNomeJogo(id, nome):
-    cursor.execute("update jogos set nome = %s where id = %s;",(nome, id))
-    conn.commit()
-def atualizaPublicadoraJogo(id, publicadora):
-    cursor.execute("update jogos set publicadora = %s where id = %s;",(publicadora, id))
-    conn.commit()
-def atualizaMaior18(id, maior18):
-    cursor.execute("update jogos set maior18 = %s where id = %s;",(maior18, id))
-    conn.commit()
-def atualizaGeneroJogo(id, genero):
-    cursor.execute("update jogos set genero = %s where id = %s;",(genero, id))    
-    conn.commit()
 def atualizaPrecoJogo(id, preco):
     cursor.execute("update jogos set preco = %s where id = %s;",(preco, id))    
     conn.commit()
+    
 def achaJogos():
     cursor.execute("select * from jogos;")
     return cursor.fetchall()
-def achaJogo(nome):
+
+def achaJogoNome(nome):
     cursor.execute("select * from jogos where nome=%s;",(nome, ))
-    return cursor.fetchall()
+    return cursor.fetchone()
+
+def achaJogoId(id):
+    cursor.execute("select * from jogos where id=%s;",(id, ))
+    return cursor.fetchone()
+
 def achaJogoNomeParecido(nome):
     #caso 2 jogos tenham mesmo nome, ie street fighter 2 e street fighter 2 turbo edition
     cursor.execute("select * from jogos where position(%s in nome)>0;",(nome, ))
     return cursor.fetchall()
+def deletaJogo(id):
+    cursor.execute("delete from jogos where id=%s;",(id, ))
+    conn.commit()
 
-#compra
+#compras
 def insereCompra(idcliente, idjogo, datacompra):
     cursor.execute("insert into compra (idcliente, idjogo, datacompra) values (%s, %s, %s);",(idcliente, idjogo, datacompra))
     conn.commit()
